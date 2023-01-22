719371e1feeb195089599026f635824d746024f2
Eduardo Garcia <edd.grc@gmail.com>
2023-01-22 11:55:45 -0800

    feat. powershell for ! commands

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -28,9 +28,16 @@ opt.hlsearch = true
 opt.incsearch = true
 
 -- User powershell
+vim.opt.shell = 'pwsh'
+vim.opt.shellcmdflag =
+[[-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;]]
+vim.opt.shellredir = [[2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode]]
+vim.opt.shellpipe = [[2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode]]
+vim.opt.shellquote=[[]]
+vim.opt.shellxquote=[[]]
 
 --[[
-                          USER INTERFACE
+  USER INTERFACE
 --]]
 -- Cursor
 opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20'
