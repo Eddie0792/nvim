f740cf3e69a22a889a9575bb699ff35cc822c781
Eduardo Garcia <edd.grc@gmail.com>
2023-01-29 12:09:25 -0800

    options.lua - Removed powershell for troubleshooting null-ls - Added prettierd formatter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -28,13 +28,13 @@ opt.hlsearch = true
 opt.incsearch = true
 
 -- User powershell
-vim.opt.shell = 'pwsh'
-vim.opt.shellcmdflag =
-[[-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;]]
-vim.opt.shellredir = [[2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode]]
-vim.opt.shellpipe = [[2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode]]
-vim.opt.shellquote=[[]]
-vim.opt.shellxquote=[[]]
+--vim.opt.shell = 'pwsh'
+--vim.opt.shellcmdflag =
+--[[-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;]]
+--vim.opt.shellredir = [[2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode]]
+--vim.opt.shellpipe = [[2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode]]
+--vim.opt.shellquote=[[]]
+--vim.opt.shellxquote=[[]]
 
 --[[
   USER INTERFACE
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,9 +1,13 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
-	dependencies = "nvim-lua/plenary.nvim",
-	config = function()
-		require("null-ls").setup({
-			sources = require("null-ls").builtins.formatting.stylua,
-		})
-	end,
+  'jose-elias-alvarez/null-ls.nvim',
+  dependencies = 'nvim-lua/plenary.nvim',
+  config = function()
+    local b = require('null-ls').builtins
+    require('null-ls').setup {
+      sources = {
+        b.formatting.stylua,
+        b.formatting.prettierd,
+      },
+    }
+  end,
 }
