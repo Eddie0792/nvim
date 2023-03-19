e955e96f120191a70829fffb0e1d33d54f443230
Eddie0792 <edd.grc@gmail.com>
2023-03-19 07:57:25 -0700

    Created bootup logic to select different init files based on operating system detected

---
+++ lua/config/unix_opts.lua
@@ -0,0 +1,81 @@
+local g = vim.g
+local opt = vim.opt
+
+-- System
+opt.confirm = true -- confirm to save changes on exit modified buffer
+opt.scrolloff = 8
+opt.showmode = false
+opt.updatetime = 250 -- Swap file write interval, also used by gitsigns
+opt.timeoutlen = 400
+opt.termguicolors = true
+opt.swapfile = true
+opt.backup = true
+opt.undofile = true
+opt.clipboard = 'unnamedplus'
+opt.mouse = 'a'
+opt.backupdir = '~/.nvim-backups'
+
+-- Disabling netrw while i test nvimtree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Blankline stuffs
+vim.opt.list = true
+
+-- Search
+opt.ignorecase = true
+opt.hlsearch = true
+opt.incsearch = true
+
+-- User powershell
+--vim.opt.shell = 'pwsh'
+--vim.opt.shellcmdflag =
+--[[-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;]]
+--vim.opt.shellredir = [[2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode]]
+--vim.opt.shellpipe = [[2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode]]
+--vim.opt.shellquote=[[]]
+--vim.opt.shellxquote=[[]]
+
+--[[
+  USER INTERFACE
+--]]
+-- Cursor
+opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20'
+opt.cursorline = true
+opt.rnu = false
+opt.colorcolumn = '80'
+
+-- Not sure what to call this group
+opt.splitbelow = true
+opt.splitright = true
+
+-- Numbers
+opt.signcolumn = 'yes'
+opt.number = true
+opt.numberwidth = 2
+opt.ruler = false
+
+-- not too sure
+-- End of buffer?
+opt.fillchars = { eob = ' ' }
+
+-- Statusline
+opt.laststatus = 3 -- Global statusline
+
+-- Indenting
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+
+opt.wrap = false
+opt.smartindent = true
+
+-- Explorer
+-- :h netrw-browser-var
+--g.netrw_winsize = 15
+--g.netrw_liststyle = 3 -- Tree style directory
+--g.netrw_browse_split = 4
+--g.netrw_liststyle = 3
+--g.netrw_banner = 0
+--g.netrw_usetab = 1 -- bind ctrl-tab to collapse netrw
